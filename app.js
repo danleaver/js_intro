b6bdd24809c5fef40027a670e47e4a0f1bf7c60f
Danny Leaver <dleaver@gmail.com>
2020-06-09 20:26:48 -0600

    js intro code

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("taco").style
+document.getElementById('btn').addEventListener("click", changePic)
+
+
+    function changePic() {
+        document.getElementById("taco").src = 'https://images.unsplash.com/photo-1508154048109-de555266b70a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80'
+      }
